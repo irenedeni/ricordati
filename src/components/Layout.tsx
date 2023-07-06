1244a224c201a2bc30c938cb7e1b452585b14972
Irene De Nicolo <irene.denicolo89@gmail.com>
2023-07-06 12:32:47 +0200

    edited file structure, added format script, added Layout component with framer motion

---
+++ src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+
+const variants = {
+  hidden: { opacity: 0, x: 0, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: 0 },
+}
+
+type Props = {
+  children: ReactNode
+  className?: string
+}
+
+const Layout = ({ children, className }: Props): JSX.Element => (
+  <>
+    <motion.main
+      variants={variants} // Pass the variant object into Framer Motion
+      initial="hidden" // Set the initial state to variants.hidden
+      animate="enter" // Animated state to variants.enter
+      exit="exit" // Exit state (used later) to variants.exit
+      transition={{ type: 'linear' }} // Set the transition to linear
+      className={className}
+    >
+      {children}
+    </motion.main>
+  </>
+)
+
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Layout from '@/components/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,15 +11,18 @@ export default function Home() {
     <>
       <Head>
         <title>Ricordati - an app to track lended and borrowed items</title>
-        <meta name="description" content="Ricordati - an app to track lended and borrowed items" />
+        <meta
+          name="description"
+          content="Ricordati - an app to track lended and borrowed items"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          ciao
-        </div>
-      </main>
+      {/* <main className={styles.main}> */}
+      <Layout className={styles.main}>
+        <div className={styles.description}>ciao</div>
+      </Layout>
+      {/* </main> */}
     </>
   )
 }
