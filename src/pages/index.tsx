40dd2fbf7b86bd2d73f4d200902db13ce03ec703
Irene De Nicolo <irene.denicolo89@gmail.com>
2023-07-06 14:10:27 +0200

    improved on global styles and general structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Layout.module.css'
 import Layout from '@/components/Layout'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -18,11 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      {/* <main className={styles.main}> */}
-      <Layout className={styles.main}>
+      <Layout className={styles.layout}>
         <div className={styles.description}>ciao</div>
       </Layout>
-      {/* </main> */}
     </>
   )
 }
