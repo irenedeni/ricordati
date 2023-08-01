a7d4635bd94507ec46b143b902d94dbc592e4b33
Irene De Nicolo <irene.denicolo89@gmail.com>
2023-08-01 17:24:51 +0200

    edited type name

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,11 +1,11 @@
 import styles from '@/styles/Divider.module.css'
 
-type Divider = {
+type DividerSize = {
   large?: boolean
 }
 
 
-const Divider = ({ large }: Divider): React.JSX.Element => {
+const Divider = ({ large }: DividerSize): React.JSX.Element => {
   return (
     <div className={large ? styles.large : styles.small}>
     </div>
